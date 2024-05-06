691b28c0712f8d624548100a03305403ac4efcf9
Carolina  Monzó <carolmonzoc@gmail.com>
2024-05-06 17:30:07 +0200

    still working on clustering

--- PCA.R
+++ PCA.R
@@ -12,12 +12,12 @@ library(pbmcMultiome.SeuratData)
 
 set.seed(000)
 
-sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_scMOSim_2groups_000.rds")
+sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_scMOSim_few_2602.rds")
 
-numcells <- "many_ATAC_G2R1"
+numcells <- "few_RNA_G1R1"
 
 # Normalize using Seurat's method
-dat <- Seurat::CreateSeuratObject(sim$Group_2$Rep_1$`sim_scATAC-seq`@counts)
+dat <- Seurat::CreateSeuratObject(sim$Group_1$Rep_1$`sim_scRNA-seq`@counts)
 dat <- Seurat::SCTransform(dat, verbose = FALSE, return.only.var.genes = FALSE)
 
 # Create cell-tocelltype ID table
@@ -45,3 +45,101 @@ fviz_eig(pca_sc)
 ggsave(width = 3, height = 2, filename = paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/", numcells, "PCA_scree.pdf"))
 
 
+
+## Now K-means starting from the log2 normalized
+asocG2 <- sim$AssociationMatrices$AssociationMatrix_Group_1[sim$AssociationMatrices$AssociationMatrix_Group_1$Gene_cluster %in% c(1:8),]
+data <- as.data.frame(dat[["SCT"]]$data)
+data <- data[rownames(data) %in% asocG2$Gene_ID,]
+
+coexpr.scaled.celltype <- data
+
+calculate_mean_per_list_df <- function(df, named_lists) {
+  means <- list()
+  for (name in names(named_lists)) {
+    columns <- named_lists[[name]]
+    means[[name]] <- rowMeans(df[, columns, drop = FALSE])
+  }
+  # Combine the list of means into a dataframe
+  means_df <- do.call(cbind, means)
+  # Add column names
+  colnames(means_df) <- names(named_lists)
+  return(means_df)
+}
+
+cell_types <- sim$cellTypes
+
+coexpr.scaled.celltype <- calculate_mean_per_list_df(coexpr.scaled.celltype, cell_types)
+
+max_itr <-  50
+n_clust  <-  8  ## number of cluster 
+
+## Remove NAs
+
+coexpr.scaled.celltype[is.na(coexpr.scaled.celltype)] <- 0
+
+kmeans_out  <- kmeans(as.matrix(coexpr.scaled.celltype),n_clust,iter.max = max_itr)
+# With cluster info from the kmeans
+
+data_with_cust_info <- as.data.frame(coexpr.scaled.celltype) %>% 
+  mutate(clust = paste("clust_", kmeans_out$cluster,sep = ""))
+
+mean_expression <- data_with_cust_info %>%
+  group_by(clust) %>%
+  summarise_all(mean)
+
+## visualise  each cluster 
+
+df <- mean_expression %>% gather(key = "variable", value = "value", -c(1)) %>%  ###the last column is the cluster
+  group_by(variable) %>%  
+  mutate(row_num =  1:n()) 
+
+
+data_with_cust_info %>% 
+  gather(key = "variable" , value = "value", -c(ncol(data_with_cust_info))) %>%  ###the last column is the cluster
+  group_by(variable) %>%  
+  mutate(row_num =  1:n()) %>% 
+  ggplot(aes(x =  variable , y = value , group = row_num)) +   
+  geom_line(alpha = 1, color = "grey") + 
+  geom_line(data = df, aes(x = variable, y= value, group = row_num), linewidth = 1, color = "red3") +
+  theme_bw() +  
+  theme(legend.position = "none" , axis.text.x = element_text(angle = 45 , vjust = 0.4)) +
+  facet_wrap(~clust, ncol = 4)
+ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/few_2A", numcells, "_kmeans.pdf"))
+
+
+## Now let's check the cluster assignment from kmeans compared to the forced cluster
+# in the association matrix
+
+data <- as.data.frame(dat[["SCT"]]$data)
+data <- calculate_mean_per_list_df(data, cell_types)
+data <- as.data.frame(data[rownames(data) %in% asocG2$Gene_ID,])
+
+subAsoc <- asocG2[, c("Gene_ID", "Gene_cluster")]
+subAsoc <- subAsoc %>% mutate(ori_clust = paste0("ori_clust_", asocG2$Gene_cluster))
+subAsoc <- subAsoc[, c("Gene_ID", "ori_clust")]
+
+data$Gene_ID <- row.names(data)
+data_with_oricust_info <- merge(data, subAsoc, by = "Gene_ID")
+data_with_oricust_info$Gene_ID <- NULL
+
+mean_expression <- data_with_oricust_info %>%
+  group_by(ori_clust) %>%
+  summarise_all(mean)
+
+## visualise  each cluster 
+
+df <- mean_expression %>% gather(key = "variable", value = "value", -c(1)) %>%  ###the last column is the cluster
+  group_by(variable) %>%  
+  mutate(row_num =  1:n()) 
+
+data_with_oricust_info %>% 
+  gather(key = "variable" , value = "value", -c(ncol(data_with_oricust_info))) %>%  ###the last column is the cluster
+  group_by(variable) %>%  
+  mutate(row_num =  1:n()) %>% 
+  ggplot(aes(x =  variable , y = value , group = row_num)) +   
+  geom_line(alpha = 1, color = "grey") + 
+  geom_line(data = df, aes(x = variable, y= value, group = row_num), linewidth = 1, color = "red3") +
+  theme_bw() +  
+  theme(legend.position = "none" , axis.text.x = element_text(angle = 45 , vjust = 0.4)) +
+  facet_wrap(~ori_clust, ncol = 4)
+ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/original_few_2A_", numcells, "_kmeans.pdf"))
--- paper_plots_sc.R
+++ paper_plots_sc.R
@@ -1,3 +1,5 @@
+setwd("~/workspace/1_conesalab/MOSim")
+devtools::load_all()
 suppressPackageStartupMessages({
   library(tidyverse)
   library(scater)
@@ -10,13 +12,15 @@ suppressPackageStartupMessages({
 set.seed(000)
 # ## Plots for paper
 
-sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_scMOSim_few_2602.rds")
+#sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_scMOSim_few_2602.rds")
 
-sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_scMOSim_2groups_000.rds")
+#sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_scMOSim_2groups_000.rds")
+
+sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_7cells8clus_scMOSim_2groups_001.rds")
 
 numcells <- "many_RNA_G1R1"
 results <- scOmicResults(sim)
-settings <- scOmicSettings(sim)
+settings <- scOmicSettings(sim, TF = TRUE)
 
 ## SC plots for paper
 # Figure 2A representation of k-means clusters for scRNAseq data forced to 
@@ -25,11 +29,11 @@ settings <- scOmicSettings(sim)
 # Extract group one
 data <- as.data.frame(results$Group_1$Rep_1$`sim_scRNA-seq`@counts)
 # Keep only genes in co-expression clusters
-asocG1 <- settings$AssociationMatrix_Group_1[settings$AssociationMatrix_Group_1$Gene_cluster %in% c(1:14),]
+asocG1 <- settings$AssociationMatrix_Group_1[settings$AssociationMatrix_Group_1$Gene_cluster %in% c(1:9),]
 data <- data[asocG1$Gene_ID,]
 # Scale data and make means per celltype
 coexpr.scaled <- acorde::scale_isoforms(data, isoform_col = NULL)
-coexpr.scaled[is.na(coexpr.scaled)] <- 0
+#coexpr.scaled[is.na(coexpr.scaled)] <- 0
 
 # Remove the transcripts names so we can work on the matrix
 coexpr.scaled.celltype <- coexpr.scaled %>% select(-transcript)
@@ -51,13 +55,11 @@ cell_types <- sim$cellTypes
 
 coexpr.scaled.celltype <- calculate_mean_per_list_df(coexpr.scaled.celltype, cell_types)
 
-#rownames(coexpr.scaled.celltype) <- coexpr.scaled$transcript
 
 ### kmeans
 max_itr <-  50
-n_clust  <-  14  ## number of cluster 
-set.seed(123) ## reproduce the cluster 
-kmeans_out  <- kmeans(coexpr.scaled.celltype,n_clust,iter.max = max_itr)
+n_clust  <-  9  ## number of cluster 
+kmeans_out  <- kmeans(as.matrix(coexpr.scaled.celltype),n_clust,iter.max = max_itr)
 # With cluster info from the kmeans
 
 data_with_cust_info <- as.data.frame(coexpr.scaled.celltype) %>% 
@@ -80,14 +82,45 @@ data_with_cust_info %>%
   mutate(row_num =  1:n()) %>% 
   ggplot(aes(x =  variable , y = value , group = row_num)) +   
   geom_line(alpha = 1, color = "grey") + 
-  geom_line(data = df, aes(x = variable, y= value, group = row_num), size = 1.5, color = "red3") +
+  geom_line(data = df, aes(x = variable, y= value, group = row_num), size = 1, color = "red3") +
   theme_bw() +  
   theme(legend.position = "none" , axis.text.x = element_text(angle = 45 , vjust = 0.4)) +
   facet_wrap(~clust, ncol = 3)
 ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/many_2A_", numcells, "_kmeans.pdf"))
 
+
+#####################
+
+
 ## Now let's check the cluster assignment from kmeans compared to the forced cluster
 # in the association matrix
 
+data_with_oricust_info <- as.data.frame(coexpr.scaled.celltype) %>% 
+  mutate(ori_clust = paste0("ori_clust_", asocG1$Gene_cluster))
+
+mean_expression <- data_with_oricust_info %>%
+  group_by(ori_clust) %>%
+  summarise_all(mean)
+
+## visualise  each cluster 
+
+df <- mean_expression %>% gather(key = "variable", value = "value", -c(1)) %>%  ###the last column is the cluster
+  group_by(variable) %>%  
+  mutate(row_num =  1:n()) 
+
+data_with_oricust_info %>% 
+  gather(key = "variable" , value = "value", -c(ncol(data_with_oricust_info))) %>%  ###the last column is the cluster
+  group_by(variable) %>%  
+  mutate(row_num =  1:n()) %>% 
+  ggplot(aes(x =  variable , y = value , group = row_num)) +   
+  geom_line(alpha = 1, color = "grey") + 
+  geom_line(data = df, aes(x = variable, y= value, group = row_num), size = 1, color = "red3") +
+  theme_bw() +  
+  theme(legend.position = "none" , axis.text.x = element_text(angle = 45 , vjust = 0.4)) +
+  facet_wrap(~ori_clust, ncol = 3)
+ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/original_many_2A_", numcells, "_kmeans.pdf"))
 
 
+
+#Extract the association matrix count the number of times the cluster is the same as it should be
+#and the number of times it is not.
