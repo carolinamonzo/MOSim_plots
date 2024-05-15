2babc42cc6eb113cd082094efb5b9937b1cdb451
Carolina  Monzó <carolmonzoc@gmail.com>
2024-05-15 23:59:33 +0200

    testing clustering

--- testing_logvsscaled_spearmanvsKendall.R
+++ testing_logvsscaled_spearmanvsKendall.R
@@ -7,6 +7,7 @@ suppressPackageStartupMessages({
   library(Seurat)
   library(RColorConesa)
   library(acorde)
+  library(corrr)
 })
 library(pbmcMultiome.SeuratData)
 
@@ -44,69 +45,38 @@ plot(df)
 df <- zoo(coexpr.scaled[, c(2,12, 25,37)])
 plot(df)
 
+######## ANGELES SCALING ##########
 # Make a correlation of all cells
 coexpr.scaled_corr <- coexpr.scaled %>% select(-transcript) %>% cor(method = "spearman")
 # Visualise the correlations between the first 5 samples
 coexpr.scaled_corr[1:5, 1:5]
 
-library(corrr)
+
 # While the strongest correlations are within cell-type, some celltypes are similar
 # To others, mostly because they are all lymphocytes
 rplot(coexpr.scaled_corr) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-#Make PCA
-library(factoextra)
+# Make a correlation of all cells
+coexpr.scaled_corr <- coexpr.scaled %>% select(-transcript) %>% cor(method = "kendall")
+# Visualise the correlations between the first 5 samples
+coexpr.scaled_corr[1:5, 1:5]
 
+# While the strongest correlations are within cell-type, some celltypes are similar
+# To others, mostly because they are all lymphocytes
+rplot(coexpr.scaled_corr) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-pca_sc <- prcomp(t(coexpr.scaled %>% select(-transcript)))
-pca_sc$x %>% as_tibble(rownames = "Cell") %>% full_join(ct, by = "Cell") %>% 
-  ggplot(aes(x = PC1, y = PC2, colour = cell_type)) + geom_point() + theme_classic(base_size = 10) +
-  scale_color_conesa(palette = "main")
-
-#ggsave(width = 3, height = 2, filename = paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/", numcells, "PCA_PC1_PC2.pdf"))
-
-# Since the third component is also important to visualize it
-pca_sc$x %>% as_tibble(rownames = "Cell") %>% full_join(ct, by = "Cell") %>% 
-  ggplot(aes(x = PC2, y = PC3, colour = cell_type)) + geom_point() + theme_classic(base_size = 10) +
-  scale_color_conesa(palette = "main")
-
-#ggsave(width = 3, height = 2, filename = paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/", numcells, "PCA_PC2_PC3.pdf"))
-
-fviz_eig(pca_sc)
-#ggsave(width = 3, height = 2, filename = paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/", numcells, "PCA_scree.pdf"))
-
-
-
-
-##### Doing the stuff on log1 data
-
-
-# PCA on the log1(1p) ori data
-pca_raw<- prcomp(log(t(as.data.frame(sim$Group_2$Rep_1$`sim_scRNA-seq`@counts)) + 1))
-pca_raw$x %>% as_tibble(rownames = "Cell") %>% full_join(ct, by = "Cell") %>% 
-  ggplot(aes(x = PC1, y = PC2, colour = cell_type)) + geom_point() + theme_classic(base_size = 10) +
-  scale_color_conesa(palette = "main")
+######## LOG1 ##########
 
 dat <- Seurat::CreateSeuratObject(sim$Group_2$Rep_1$`sim_scRNA-seq`@counts)
 dat <- Seurat::SCTransform(dat, verbose = FALSE, return.only.var.genes = FALSE)
 
-pca_sc <- prcomp(t(dat[["SCT"]]$data))
-pca_sc$x %>% as_tibble(rownames = "Cell") %>% full_join(ct, by = "Cell") %>% 
-  ggplot(aes(x = PC1, y = PC2, colour = cell_type)) + geom_point() + theme_classic(base_size = 10) +
-  scale_color_conesa(palette = "main")
-
-library(factoextra)
-fviz_eig(pca_raw)
-fviz_eig(pca_sc)
-
-
-# MAKE HEATMAPS OF CORRELATION BETWEEN CELLS
 coexpr.log_corr <- dat[["SCT"]]$data %>% cor(method = "spearman")
 # Visualise the correlations between the first 5 cells
 coexpr.log_corr[1:5, 1:5]
 
-library(corrr)
+
 # While the strongest correlations are within cell-type, some celltypes are similar
 # To others, mostly because they are all lymphocytes
 rplot(coexpr.log_corr) +
