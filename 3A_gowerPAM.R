c648fae6dbfa35867148164a6844d63b92651f70
Carolina  Monzó <carolmonzoc@gmail.com>
2024-05-23 15:42:17 +0200

    add plot of PAM scree

--- 3A_gowerPAM.R
+++ 3A_gowerPAM.R
@@ -11,17 +11,7 @@ suppressPackageStartupMessages({
   library(cluster)
 })
 library(pbmcMultiome.SeuratData)
-
-set.seed(000)
-
-sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_6cells11clus8800_scMOSim_2groups_000.rds")
-
-ct <- tibble::tibble(Cell = colnames(sim$Group_2$Rep_1$`sim_scRNA-seq`@counts),
-                     cell_type = rep(names(sim$cellTypes), times = lengths(sim$cellTypes)))
-
-rna <- log1p(sim$Group_2$Rep_1$`sim_scRNA-seq`@counts)
-
-atac <- log1p(sim$Group_2$Rep_1$`sim_scATAC-seq`@counts)
+library(cowplot)
 
 calculate_mean_per_list_df <- function(df, named_lists) {
   means <- list()
@@ -36,81 +26,55 @@ calculate_mean_per_list_df <- function(df, named_lists) {
   return(means_df)
 }
 
+set.seed(000)
+
+### read the simulated dataset
+
+sim <- readRDS("~/workspace/1_conesalab/test_scMOSim/paper_plots/sim_6cells11clus8800_scMOSim_2groups_000.rds")
+
+settings <- scOmicSettings(sim, TF = TRUE)
 cell_types <- sim$cellTypes
 
-means_celltype_rna <- calculate_mean_per_list_df(rna, cell_types)
-means_celltype_atac <- calculate_mean_per_list_df(atac, cell_types)
+### SCALE USING ACORDE
 
-asocG2 <- sim$AssociationMatrices$AssociationMatrix_Group_2[sim$AssociationMatrices$AssociationMatrix_Group_2$Gene_cluster %in% c(1:11),]
-means_celltype_rna <- as.data.frame(means_celltype_rna[rownames(means_celltype_rna) %in% asocG2$Gene_ID,])
-
-# Cluster our 11 clusters of interest
-rna_gower_dist <- cluster::daisy(means_celltype_rna, metric = "gower")
-rna_pam <- pam(rna_gower_dist, diss = TRUE, k = 11)
-
-pam_cluster <- as.data.frame(rna_pam$clustering)
-colnames(pam_cluster) <- c("pam_cluster")
-
-pam_cluster <- as.data.frame(pam_cluster) %>% 
-  mutate(clust = paste("clust_", pam_cluster,sep = ""))
-pam_cluster$pam_cluster <- NULL
-
-rna_with_clust_info <- merge(means_celltype_rna, pam_cluster, by = 0)
-
-rownames(rna_with_clust_info) <- rna_with_clust_info$Row.names
-rna_with_clust_info$Row.names <- NULL
-
-
-mean_expression <- rna_with_clust_info %>%
-  group_by(clust) %>%
-  summarise_all(mean)
-
-## visualise  each cluster 
-
-df <- mean_expression %>% gather(key = "variable", value = "value", -c(1)) %>%  ###the last column is the cluster
-  group_by(variable) %>%  
-  mutate(row_num =  1:n()) 
-
-
-rna_with_clust_info %>% 
-  gather(key = "variable" , value = "value", -c(ncol(rna_with_clust_info))) %>%  ###the last column is the cluster
-  group_by(variable) %>%  
-  mutate(row_num =  1:n()) %>% 
-  ggplot(aes(x =  variable , y = value , group = row_num)) +   
-  geom_line(alpha = 1, color = "grey") + 
-  geom_line(data = df, aes(x = variable, y= value, group = row_num), linewidth = 1, color = "red3") +
-  theme_bw() +  
-  theme(legend.position = "none" , axis.text.x = element_text(angle = 45 , vjust = 0.4)) +
-  facet_wrap(~clust, ncol = 4)
-ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/3A_gowerPAM.pdf"))
-
-## Plot this nicer for the supplementary
-# sil_width <- c(NA)
-# 
-# for(i in 2:15){
-#   
-#   pam_fit <- pam(rna_gower_dist,
-#                  diss = TRUE,
-#                  k = i)
-#   
-#   sil_width[i] <- pam_fit$silinfo$avg.width
-#   
-# }
-# 
-# plot(1:15, sil_width,
-#      xlab = "Number of clusters",
-#      ylab = "Silhouette Width")
-# lines(1:15, sil_width)
-
-
-
-####### Doing the same but scaling with acorde, looks very good
-rna <- as.data.frame(acorde::scale_isoforms(sim$Group_1$Rep_1$`sim_scRNA-seq`@counts, 
-                              isoform_col = NULL))
+rna <- acorde::scale_isoforms(sim$Group_1$Rep_1$`sim_scRNA-seq`@counts, 
+                                            isoform_col = NULL)
 
 rna[is.na(rna)] <- 0
 
-cell_types <- sim$cellTypes
+ct <- tibble::tibble(Cell = colnames(sim$Group_2$Rep_1$`sim_scRNA-seq`@counts),
+                     cell_type = rep(names(sim$cellTypes), times = lengths(sim$cellTypes)))
+
+### PLOT ORIGINAL CLUSTERS
+
+## Extract clusters list to plot originals
+clusters_list <- settings$AssociationMatrix_Group_1[settings$AssociationMatrix_Group_1$Gene_cluster %in% c(1:11), c("Gene_ID", "Gene_cluster")]
+clusters_list <- split(clusters_list$Gene_ID, clusters_list$Gene_cluster)
+
+
+# compute average-by-cell type cluster patterns
+cluster_patterns <- map(clusters_list,
+                        ~acorde::calculate_cluster_profile(rna,
+                                                           isoform_ids = .,
+                                                           id_table = ct,
+                                                           isoform_col = "transcript"))
+
+
+theme_set(theme_cowplot())
+
+pattern_plots <- map(cluster_patterns,
+                     plot_cluster_profile,
+                     ct_labels = c("CD16 Mono", "NK", "Treg", "cDC", "CD4 TEM", "Memory B"))
+
+plot_grid(plotlist = pattern_plots, 
+          labels = NULL, 
+          ncol = 4)
+ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/Supp_OriClusters_acordeScaled.pdf"))
+
+####### Clustering genes with acorde scaling, manhattan and kmedoids
+
+
+rna <- as.data.frame(rna)
 rownames(rna) <- rna$transcript
 rna$transcript <- NULL
 
@@ -160,38 +124,26 @@ rna_with_clust_info %>%
 ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/3A_G1_gowerPAM_acordeScaled.pdf"))
 
 
-## Checking the original clustering
-means_celltype_rna_ori <- calculate_mean_per_list_df(rna, cell_types)
+##### Plot scree of the medoids
 
-asocG2 <- sim$AssociationMatrices$AssociationMatrix_Group_2[sim$AssociationMatrices$AssociationMatrix_Group_2$Gene_cluster %in% c(1:11),]
-means_celltype_rna_ori <- as.data.frame(means_celltype_rna_ori[rownames(means_celltype_rna_ori) %in% asocG2$Gene_ID,])
+sil_width <- c(NA)
+ 
+for(i in 2:15){
+  
+  pam_fit <- pam(rna_gower_dist,
+                 diss = TRUE,
+                 k = i)
+  
+  sil_width[i] <- pam_fit$silinfo$avg.width
+  
+}
 
-subAsoc <- asocG2[, c("Gene_ID", "Gene_cluster")]
-subAsoc <- subAsoc %>% mutate(ori_clust = paste0("ori_clust_", asocG2$Gene_cluster))
-subAsoc <- subAsoc[, c("Gene_ID", "ori_clust")]
+sil_width <- data.frame(
+  Position = 1:length(sil_width), 
+  Value = sil_width)
 
-means_celltype_rna_ori$Gene_ID <- row.names(means_celltype_rna_ori)
-means_celltype_rna_ori <- merge(means_celltype_rna_ori, subAsoc, by = "Gene_ID")
-means_celltype_rna_ori$Gene_ID <- NULL
+ggplot(sil_width, aes(x = Position, y = Value)) + geom_point() + geom_line() +
+  labs(x = "Number of medoids", y = "Pam average fit") + theme_classic()
+ggsave(width = 3, height = 2, filename = "~/workspace/1_conesalab/test_scMOSim/paper_plots/Kmedoids_scree.pdf")
 
-mean_expression <- means_celltype_rna_ori %>%
-  group_by(ori_clust) %>%
-  summarise_all(mean)
 
-## visualise  each cluster 
-
-df <- mean_expression %>% gather(key = "variable", value = "value", -c(1)) %>%  ###the last column is the cluster
-  group_by(variable) %>%  
-  mutate(row_num =  1:n()) 
-
-means_celltype_rna_ori %>% 
-  gather(key = "variable" , value = "value", -c(ncol(means_celltype_rna_ori))) %>%  ###the last column is the cluster
-  group_by(variable) %>%  
-  mutate(row_num =  1:n()) %>% 
-  ggplot(aes(x =  variable , y = value , group = row_num)) +   
-  geom_line(alpha = 1, color = "grey") + 
-  geom_line(data = df, aes(x = variable, y= value, group = row_num), linewidth = 1, color = "red3") +
-  theme_bw() +  
-  theme(legend.position = "none" , axis.text.x = element_text(angle = 45 , vjust = 0.4)) +
-  facet_wrap(~ori_clust, ncol = 4)
-ggsave(paste0("~/workspace/1_conesalab/test_scMOSim/paper_plots/3A_oriCluster_acordeScaling.pdf"))
